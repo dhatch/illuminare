829273516dee6f25789ce807ca48264839b4cbcf
David Hatch <dhatch387@gmail.com>
2012-05-09 17:26:43 -0400

    fix repo structure and package name.

---
+++ src/main/java/com/freedobjective/illuminare/IlluminareMain.java
@@ -0,0 +1,11 @@
+package com.freedobjective.illuminare;
+
+/**
+ * Hello world!
+ *
+ */
+public class IlluminareMain {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
--- src/test/java/com/freedobjective/illuminare/AppTest.java
+++ src/test/java/com/freedobjective/illuminare/AppTest.java
@@ -1,4 +1,4 @@
-package illuminare;
+package com.freedobjective.illuminare;
 
 import junit.framework.Test;
 import junit.framework.TestCase;
