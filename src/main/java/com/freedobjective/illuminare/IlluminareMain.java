db1d0e7b31e2e9c026ff50bb4ba6c3e6a3716d08
David Hatch <dhatch387@gmail.com>
2012-05-10 12:32:58 -0400

    set up display.

--- src/main/java/com/freedobjective/illuminare/IlluminareMain.java
+++ src/main/java/com/freedobjective/illuminare/IlluminareMain.java
@@ -1,11 +1,37 @@
 package com.freedobjective.illuminare;
 
-/**
- * Hello world!
- *
- */
+import org.lwjgl.opengl.Display;
+import org.lwjgl.opengl.DisplayMode;
+import org.lwjgl.LWJGLException;
+
 public class IlluminareMain {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    
+    public static IlluminareMain app;
+    
+    public IlluminareMain() throws LWJGLException {
+        // Initialize display
+        DisplayMode m = Display.getDesktopDisplayMode();
+        for (DisplayMode mode: Display.getAvailableDisplayModes()) {
+            // Select this mode to start with
+            if (mode.getWidth() == 1024 && mode.getHeight() == 768) {
+                m = mode;
+                break;
+            }
+        }
+        Display.setDisplayMode(m);
+        Display.setTitle("Illuminare");
+        Display.create();
     }
+    
+    public static void main(String[] args) throws LWJGLException {
+        IlluminareMain app = getApp();
+    }
+    
+    public static IlluminareMain getApp() throws LWJGLException {
+        if (app == null) {
+            app = new IlluminareMain();
+        }
+        return app;
+    }
+    
 }
